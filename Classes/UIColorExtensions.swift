dc309a1927f157b4bfcb379b2f507ff75e83c964
Tomer Buzaglo <iTomer@Tomers-MacBook-Pro.local>
2015-08-02 12:13:54 +0300

    fixed a bug

--- Classes/UIColorExtensions.swift
+++ Classes/UIColorExtensions.swift
@@ -7,9 +7,9 @@ public extension UIColor{
     //Init with RGB Colors ranging from 0-255.
     public convenience init(red:Int, green:Int, blue:Int){
         
-        self.init(red:CGFloat(CGFloat(red/255)),
-                  green:CGFloat(CGFloat(green/255)),
-                  blue:CGFloat(CGFloat(blue/255)),
+        self.init(red:CGFloat(CGFloat(red)/255),
+                  green:CGFloat(CGFloat(green)/255),
+                  blue:CGFloat(CGFloat(blue)/255),
                   alpha:CGFloat(1)
         )
     }
