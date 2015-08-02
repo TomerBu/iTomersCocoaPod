cde4ff8d0562c5f8e105962008019027d1edeff3
Tomer Buzaglo <iTomer@Tomers-MacBook-Pro.local>
2015-08-02 10:50:31 +0300

    added an extenssion

--- Classes/UIColorExtensions.swift
+++ Classes/UIColorExtensions.swift
@@ -1,7 +1,8 @@
 import UIKit
 
 extension UIColor{
-  var test:String {
-    return "test worked"
-  }
+    public convenience init(red:Int, green:Int, blue:Int){
+        self.init(red:CGFloat(CGFloat(red/255)),green:CGFloat(CGFloat(green/255)),blue:CGFloat(CGFloat(blue/255)),alpha:CGFloat(1))
+    }
+    
 }
